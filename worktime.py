974881f64d505e04467c43e2111f0c4d1cd48e7c
Artur Majewski <artur.majewski@gecko-lab.com>
2016-02-05 13:46:02 +0100

    Initial

---
+++ worktime.py
@@ -0,0 +1,34 @@
+#!/usr/local/bin/python3
+from time import gmtime, strftime
+from datetime import datetime
+import shlex
+import os
+from subprocess import call
+
+var = os.popen("pmset -g log | grep -e \"PowerButton\" -e \"Start\" | tail -1| awk {'print $1 \" Start time: \" $2 \" \"'} ").read();
+# dobra, teraz trza by to troche porozdzielac na poziomie skryptu...
+
+var = var.rstrip() # remove newlines
+begindate = var.split()[0]
+print("Work begin hour for day " + begindate + ":" )
+begintime = var.split()[3] # extract the exact time in dd:dd:dd format
+beginhour = begintime.split(":")[0]
+beginmin = begintime.split(":")[1]
+beginsec = begintime.split(":")[2]
+print(""+beginhour+":"+beginmin+":"+beginsec)
+
+currenttime = datetime.now().timetuple()
+currenthour= currenttime[3]
+currentmin = currenttime[4]
+currentsec = currenttime[5]
+currentday = currenttime[2]
+print("Current time: "+str(currenthour)+":"+str(currentmin)+":"+str(currentsec))
+print("Current date (day): " + str(currentday))
+currenttimeobj = datetime.now()
+previoustimeobj = datetime(currenttimeobj.year, currenttimeobj.month, currenttimeobj.day, int(beginhour), int(beginmin), int(beginsec))
+timediff = currenttimeobj - previoustimeobj
+print(str(timediff))
+
+filedescriptor = open("worklog.txt", 'a')
+filedescriptor.write(str(begindate)+": " + str(timediff)+"\n")
+filedescriptor.close()
