80f6182970bdc3ae0dc1e0488897756fef0be962
Artur Majewski <artur.majewski@gecko-lab.com>
2016-02-10 14:54:15 +0100

    Add seconds to the info.

--- worktime.py
+++ worktime.py
@@ -3,11 +3,14 @@ from datetime import datetime
 import os
 
 var = os.popen("pmset -g log | grep -e \"PowerButton\" -e \"Start\" | tail -1| awk {'print $1 \" Start time: \" $2 \" \"'} ").read();
+# samo pmset log, jak zemulować jebane awk?
+
 # dobra, teraz trza by to troche porozdzielac na poziomie skryptu...
 
 var = var.rstrip() # remove newlines
 begindate = var.split()[0]
 print("Work begin hour for day " + begindate + ":" )
+
 begintime = var.split()[3] # extract the exact time in dd:dd:dd format
 beginhour = begintime.split(":")[0]
 beginmin = begintime.split(":")[1]
@@ -21,6 +24,7 @@ currentsec = currenttime[5]
 currentday = currenttime[2]
 print("Current time: "+str(currenthour)+":"+str(currentmin)+":"+str(currentsec))
 print("Current date (day): " + str(currentday))
+
 currenttimeobj = datetime.now()
 previoustimeobj = datetime(currenttimeobj.year, currenttimeobj.month, currenttimeobj.day, int(beginhour), int(beginmin), int(beginsec))
 timediff = currenttimeobj - previoustimeobj
