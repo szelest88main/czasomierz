b16b4d9ff5b66fe6a138816f675a4f7c3a933751
Artur Majewski <artur.majewski@gecko-lab.com>
2016-02-24 09:33:30 +0100

    WIP script for compacting data

--- compact.py
+++ compact.py
@@ -1,9 +1,27 @@
 #!/usr/local/bin/python3
 import os
+import re
 filedescriptor = open("test.txt", 'r')
 lines = filedescriptor.readlines()
 filedescriptor.close()
 for line in lines:
 	print(line,end="")
-
-
+lines_compacted = []
+for line in lines:
+	if "d" in line:
+		print(line.strip()+" contains d\n", end="")
+	else:
+		print(line.strip()+" does not\n", end = "")
+		
+#while hasNext:
+#	if ("d" in lines[i]):
+#		lines_compacted
+print("Now lines in array using for loop")
+for x in range(0, len(lines)):
+	line = lines[x].strip()
+	print(line)
+	
+	p = re.compile(r'*szata*')
+	searchObj = p.search(str(line))
+	print("Match: "+str(searchObj.groups()))
+	
