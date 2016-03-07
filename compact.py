dfa31e69d1c4488fdedbb018df46e39038589509
Artur Majewski <artur.majewski@gecko-lab.com>
2016-03-07 15:17:49 +0100

    Fix some strange bahviours (now the algorithm should be correct...)

--- compact.py
+++ compact.py
@@ -4,9 +4,11 @@ import re
 filedescriptor = open("test.txt", 'r')
 lines = filedescriptor.readlines()
 filedescriptor.close()
+print("The file:")
 for line in lines:
 	print(line,end="")
 lines_compacted = []
+print("The file and containing test:");
 for line in lines:
 	if "d" in line:
 		print(line.strip()+" contains d\n", end="")
@@ -16,12 +18,23 @@ for line in lines:
 #while hasNext:
 #	if ("d" in lines[i]):
 #		lines_compacted
-print("Now lines in array using for loop")
+print("Now lines in array using for loop:")
 for x in range(0, len(lines)):
+	#print("processing"+str(x)+":"+lines[x].strip());
 	line = lines[x].strip()
-	#print(line)
+#	print("processed line:"+line)
 #	print("Splited line:" + str(line.split()));
 #	print("Splited line components:" + str(len(line.split())));
-	if(len(lines[x-1].split())>0 and len(line.split())>0):
-		if(x!=0 and lines[x-1].split()[0]!=line.split()[0]):
-			print(lines[x-1].strip())	
+		#print(line.strip()+" is ok");
+	currentline = line.split()[0];
+	previousline = "";
+#	if(x==0 && lines // jeśli jest ppierwszy (0) i inny niż kolejny, daj go
+	lastline = "";
+	if(x!=0):	
+		previousline = lines[x-1].split()[0];
+	if(x!=0 and (currentline!=previousline)):
+		print(lines[x-1].strip())	
+		lastline = lines[x-1].strip()
+
+	if((x==len(lines)-1 and currentline!=lastline)): # nie że jest różna od poprzedniej, tylko że jest różna od ostatniej wyświetlonej (czy tam dodanej do docelowego kontenera)
+		print(lines[x].strip())
