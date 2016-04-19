e748bae3a01b075d430890b339d45a4cb1af49d5
Artur Majewski <artur.majewski@gecko-lab.com>
2016-04-19 15:18:30 +0200

    Add some doodles on overriding the start hour via file :D

--- worktime.py
+++ worktime.py
@@ -2,6 +2,7 @@
 
 from datetime import datetime
 import os
+import re
 
 var = os.popen("pmset -g log | grep -e \"PowerButton\" -e \"Start\" | tail -1| awk {'print $1 \" Start time: \" $2 \" \"'} ").read();
 # samo pmset log, jak zemulować jebane awk?
@@ -9,38 +10,45 @@ var = os.popen("pmset -g log | grep -e \"PowerButton\" -e \"Start\" | tail -1| a
 # dobra, teraz trza by to troche porozdzielac na poziomie skryptu...
 
 def getFile(filename):
-           filedescriptor = open(filename, 'r')
-           lines = filedescriptor.readlines()
-           filedescriptor.close()
-           return lines;
+	filedescriptor = open(filename, 'r')
+	lines = filedescriptor.readlines()
+	filedescriptor.close()
+	return lines;
 
+
+FORCE_READ_FROM_FILE = True # make it possible to set it via param
+
+if(FORCE_READ_FROM_FILE):
+	print("WARNING! FORCE_READ_FROM_FILE FLAG ENABLED!");
 data_read_from_file = False
 var = var.rstrip() # remove newlines
-begindate = var.split()[0]
-print("Work begin hour for day " + begindate + ":" )
-day_from_begindate = begindate.split("-")[2]
-begintime = var.split()[3] # extract the exact time in dd:dd:dd format
-beginhour = begintime.split(":")[0]
-beginmin = begintime.split(":")[1]
-beginsec = begintime.split(":")[2]
-print(""+beginhour+":"+beginmin+":"+beginsec)
-
+log_read_corrupted = False
+# do print beginhour w try catch i w catch: daymatches = False
+try:
+	begindate = var.split()[0]
+	day_from_begindate = begindate.split("-")[2]
+	begintime = var.split()[3] # extract the exact time in dd:dd:dd format
+	beginhour = begintime.split(":")[0]
+	beginmin = begintime.split(":")[1]
+	beginsec = begintime.split(":")[2]
+except IndexError:
+	log_read_corrupted = True
 currenttime = datetime.now().timetuple()
 currenthour= currenttime[3]
 currentmin = currenttime[4]
 currentsec = currenttime[5]
 currentday = currenttime[2]
-
-print("Current time: "+str(currenthour)+":"+str(currentmin)+":"+str(currentsec))
-print("Current date (day): " + str(currentday))
+currentmonth = currenttime[1]
 
 daymatches = False
 testingMode = False
 
+timediff = -1
+
 if (testingMode == True):
 	print("------------====== WARNING: testingMode ENABLED ======--------------")
 
-if currentday==int(day_from_begindate):
+if ((not log_read_corrupted) and currentday==int(day_from_begindate)):
 	daymatches = True
 	print("Day matches");
 else:
@@ -48,30 +56,38 @@ else:
 
 if (testingMode == True):
 	daymatches = False
-
-if(daymatches == False):
-	print("day doesnt match, trying to read from file...")
+file_data_has_been_read = False
+currenttimeobj = datetime.now()
+if(daymatches == False or log_read_corrupted == True or FORCE_READ_FROM_FILE):
+	print("day doesnt match (or forced to read from file via parameter), trying to read from file...")
 	overridingDates = getFile("overriding_dates.txt")
 	if(len(overridingDates)>0): # line format: 2016-03-10 09:00:00
-		print(str(overridingDates[0]))
 		for x in range(0, len(overridingDates)):
 			line = overridingDates[x].strip()
-			currentline = line.split()[0]
-			if(currentline.split(":")=="oo"):
-				print("oo detexted")
+			currentline = line
+			currentday_read = int(re.split('[- :]', currentline)[2])
+#			print("day:"+str(currentday_read))
+			currentmonth_read = int(re.split('[-: ]', currentline)[1])
+			if(currentday == currentday_read and currentmonth == currentmonth_read):
+				file_data_has_been_read = True
+				beginhour=int(re.split('[- :]',currentline)[3])	
+				beginmin=int(re.split('[- :]',currentline)[4])
+				beginsec=int(re.split('[- :]', currentline)[5])
+				print("found overridingData="+str(beginhour)+":"+str(beginmin))
+				previoustimeobj = datetime(currenttimeobj.year, currenttimeobj.month, currenttimeobj.day, int(beginhour), int (beginmin), 0)
+#			if(currentda
 	else:
 		print("failed (no entries)")
-currenttimeobj = datetime.now()
-
-previoustimeobj = datetime(currenttimeobj.year, currenttimeobj.month, currenttimeobj.day, int(beginhour), int(beginmin), int(beginsec))
-timediff = currenttimeobj - previoustimeobj
-print("NORMAL RESULT: "+str(timediff))
+if(log_read_corrupted == False):
+	previoustimeobj = datetime(currenttimeobj.year, currenttimeobj.month, currenttimeobj.day, int(beginhour), int(beginmin), int(beginsec))
+	timediff = currenttimeobj - previoustimeobj
+	print("NORMAL RESULT: "+str(timediff))
 
 if(testingMode ==True):
 	daymatches=False
 
 manualSelection = 'x'
-if (daymatches==False or testingMode==True):
+if ((daymatches==False or testingMode==True) and (not file_data_has_been_read)):
 	print("Day does not match 2!");
 	manualSelection = input ("Do you want to select the start time manually?");
 	if(manualSelection=='y' or manualSelection=='Y'):
@@ -86,11 +102,11 @@ if (daymatches==False or testingMode==True):
 		print("Chose: other than yes");
 		print("Leaving the script");
 
-if (daymatches==False and (manualSelection=='y' or manualSelection=='Y')):
+if (daymatches==False and (file_data_has_been_read ==True or manualSelection=='y' or manualSelection=='Y')):
 	timediff = currenttimeobj - previoustimeobj
 	print ("Corrected time:")
 	print(str(timediff))
-
-filedescriptor = open("log.txt", 'a')
-filedescriptor.write(str(begindate)+": " + str(timediff)+"\n")
-filedescriptor.close()
+if (not timediff==-1):
+	filedescriptor = open("log.txt", 'a')
+	filedescriptor.write(str(currenttimeobj.year).zfill(2)+"-"+str(currenttimeobj.month).zfill(2)+"-"+str(currenttimeobj.day).zfill(2)+": " + str(timediff)+"\n")
+	filedescriptor.close()
